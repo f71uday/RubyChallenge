36866318fea06cae8c9ffb09a9accda033672e8f
uday singh <f71uday@gmail.com>
2017-07-06 15:54:44 +0530

    initial commit

---
+++ array_selection.rb
@@ -0,0 +1,25 @@
+def select_arr(arr)
+  # select and return all odd numbers from the Array variable `arr`
+    arr.select do |a|
+        a%2==1
+    end
+end
+
+def reject_arr(arr)
+  # reject all elements which are divisible by 3
+    arr.reject do |a|
+        a/3==0
+    end
+end
+
+def delete_arr(arr)
+  # delete all negative elements
+    arr.delete_if {|a| a<0}
+    
+    
+end
+
+def keep_arr(arr)
+  # keep all non negative elements ( >= 0)
+    arr.keep_if {|a| a>=0}
+end
